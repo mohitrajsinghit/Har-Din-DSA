c39c10f1783e9047cb07833be814776af22a3487
mohitsingh10 <113434592+mohitsingh10@users.noreply.github.com>
2024-02-01 19:12:28 +0530

    Left_Rotate_by_K_Elements.cpp

---
+++ Arrays/Left_Rotate _by_K_Elements.cpp
@@ -0,0 +1,33 @@
+https://www.codingninjas.com/studio/problems/rotate-array_1230543?leftPanelTabValue=SUBMISSION
+BRUTEFORCE with TLE
+vector<int> rotateArray(vector<int>arr, int k) {
+    // Write your code here.
+    int temp[k];
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        temp[i] = arr[i];
+    }
+
+    for(int i=k;i<n;i++){
+        arr[i-k]=arr[i];
+    }
+    
+    for(int i=n-k;i<n;i++){
+        arr[i]=temp[i-(n-k)];
+    }
+    return arr;
+};
+
+
+https://leetcode.com/problems/rotate-array/
+OPTIMIZED CODE:-
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k),nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
