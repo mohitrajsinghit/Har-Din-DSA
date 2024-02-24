649f8be385b1b58d5a084ef26d33ff644c0c9b7a
mohitsingh10 <113434592+mohitsingh10@users.noreply.github.com>
2024-02-24 23:48:14 +0530

    valid_palindrome.cpp

--- Learn Basic Recursion/valid_palindrome.cpp
+++ Learn Basic Recursion/valid_palindrome.cpp
@@ -3,7 +3,7 @@ https://leetcode.com/problems/valid-palindrome/
 class Solution {
 public:
     bool isPalindrome(string s) {
-
+ 
         int left = 0, right = s.length()-1;
         while(left<right)
         {
