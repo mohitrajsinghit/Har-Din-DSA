3fb6c10ab4fea0cdc1b0fd5db1ff26788c8fa97b
Mohit Raj <113434592+mohitrajsinghit@users.noreply.github.com>
2024-03-03 23:02:22 +0530

    Majority_Element.cpp

---
+++ Arrays/Majority_Element.cpp
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/majority-element/submissions/1192831959/
+
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return nums[n/2];
+    }
+};
