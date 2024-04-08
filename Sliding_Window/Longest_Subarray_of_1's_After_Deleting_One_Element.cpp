1156563a467e0897d0aa6ab069dbcd82ab68712e
Mohit Raj <113434592+mohitrajsinghit@users.noreply.github.com>
2024-04-08 23:26:57 +0530

    Longest_Subarray_of_1's_After_Deleting_One_Element.cpp

---
+++ Sliding_Window/Longest_Subarray_of_1's_After_Deleting_One_Element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int l=0,r=0,n=nums.size(),ans=0;
+        int count =0;
+        for(;r<n;r++){
+            if(nums[r]==0) count++;
+            for(;count>1;l++){ //invalid condition
+                if(nums[l]==0)count--;
+            }
+            ans = max(ans,r-l+1);
+        }
+        return ans-1;
+    }
+};
