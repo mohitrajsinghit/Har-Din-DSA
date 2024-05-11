9d67d01eb134f14153c490211b0519fd516badb4
Mohit Raj <113434592+mohitrajsinghit@users.noreply.github.com>
2024-05-11 22:46:07 +0530

    Create Insertion_Sort.cpp

---
+++ Sorting/Insertion_Sort.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n) {
+    for (int i = 0; i <= n - 1; i++) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+    }
+};
